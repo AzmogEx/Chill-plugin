ec0a0a439600e68ac83e297017008016f7a56b8c
Adam Marzuk <marzukadam@gmail.com>
2024-05-14 18:41:16 +0200

    Initial commit

---
+++ src/main/java/fr/azmog/chill/Main.java
@@ -0,0 +1,9 @@
+package fr.azmog.chill;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends JavaPlugin {
+
+}
